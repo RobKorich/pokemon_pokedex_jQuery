9e3ece3e0dc28b29a3d9baebfa5aa00f5dd0fdaf
Rob Korich <tennisrob@gmail.com>
2020-07-24 19:12:17 -0700

    css styles/imgs/bootstrap navbar added

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,7 @@
 var pokemonRepository = (function () { /*IIFE start*/
     var pokemonList = [];
     var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150'; /*pokemon list now loaded from external api*/
-        /*{
-            name: 'Charmander',
-            height: 2, 
-            type: ['fire']
-        },
-        {
-            name: 'Pikachu', 
-            height: 1, 
-            type: ['electric']
-        },
-        {
-            name: 'Squirtle', 
-            height: 1.5, 
-            type: ['water']
-        }
-    ];*/
-
+   
     function loadList() {
       return $.ajax(apiUrl, {dataType: 'json'}).then(function(responseJSON) {
         responseJSON.results.forEach(function (item) {
@@ -32,61 +16,18 @@ var pokemonRepository = (function () { /*IIFE start*/
       });
     };
 
-    //function loadList() { 
-        //return fetch(apiUrl).then(function (response) { /*fetch function used on apiUrl var to get pokemon list / start promise for async*/
-          //return response.json(); // returns response and then takes that and converts it to json format from string
-        //}).then(function (json) { 
-          //json.results.forEach(function (item) { //explain what this line is doing please... I know it is loop start but do not get json.results and what that is
-            //var pokemon = { // sets pokemon variable to be an object with name and detailsUrl
-              //name: item.name,
-              //detailsUrl: item.url
-            //};
-            //add(pokemon); // normalize the response and store it in the pokemonList array
-          //});
-        //}).catch(function (e) { //promise catch for error response
-          //console.error(e);
-        //})
-      //}
-
-      function loadDetails(pokemon) {
-        var url = pokemon.detailsUrl;
-        return $.ajax(url, {dataType: 'json'})
-        .then(function (details) {
-          pokemon.imageUrl = details.sprites.front_default;
-          pokemon.height = details.height;
-          pokemon.types = details.types;
-          pokemon.weight = details.weight;
-        }).catch(function (e) {
-          console.error(e);
-        });
-      }
-
-    //function loadDetails(pokemon) {
-        //var url = pokemon.detailsUrl;
-        //return fetch(url).then(function (response) {
-          //return response.json();
-        //}).then(function (details) {
-          //Now we add the details to the pokemon
-          //pokemon.imageUrl = details.sprites.front_default;
-          //pokemon.height = details.height;
-          //pokemon.types = details.types;
-          //pokemon.weight = details.weight;
-        //}).catch(function (e) {
-          //console.error(e);
-        //});
-      //}
-
-    /* how to add a pokemon
-    console.log(pokemonRepository.getAll());
-
-    pokemonRepository.add({
-        name: 'Bulbasaur',
-        height: 1,
-        type: ['poison']
-    });
-
-    console.log(pokemonRepository.getAll());
-    */
+   function loadDetails(pokemon) {
+      var url = pokemon.detailsUrl;
+      return $.ajax(url, {dataType: 'json'})
+      .then(function (details) {
+        pokemon.imageUrl = details.sprites.front_default;
+        pokemon.height = details.height;
+        pokemon.types = details.types;
+        pokemon.weight = details.weight;
+      }).catch(function (e) {
+        console.error(e);
+      });
+    }
 
     function add(pokemon) {
         pokemonList.push(pokemon);
@@ -108,19 +49,16 @@ var pokemonRepository = (function () { /*IIFE start*/
       modalBody.empty();
       modalTitle.empty();
       
-      var titleElement = $('<h1>' + pokemon.name + '</h1>');
+      var titleElement = $('<h1 id="pokemonName">' + pokemon.name + '</h1>');
       modalTitle.append(titleElement);
-      //titleElement.text(pokemon.name);
-
+      
       var pictureElement = $('<img class="picture">');
       pictureElement.attr('src', pokemon.imageUrl);
 
       var heightElement = $('<p>' + 'Height: ' + pokemon.height + ' ft' + '</p>');
-      //heightElement.text('Height: ' + pokemon.height);
-     
+      
       var weightElement = $('<p>' + 'Weight: ' + pokemon.weight + ' lb' + '</p>');
-      //weightElement.text('Weight: ' + pokemon.weight);
-    
+      
       var types = [];
       var typeElement = $('<p></p>');
       $.each(pokemon.types, (i, type) => {
@@ -138,11 +76,9 @@ var pokemonRepository = (function () { /*IIFE start*/
     
     function addListItem(pokemon) {
         var pokedexList = $('.pokemon-list');
-        var listItem = $('<li></li>');
-        var button = $('<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal"></button>');
-        button.text(pokemon.name);
-        button.on('click', () => showDetails(pokemon));
-        listItem.append(button);
+        var listItem = $('<button type= "button" class="btn btn-lg btn-primary rounded-pill list-group-item" data-toggle="modal" data-target="#exampleModal"></button>');
+        listItem.text(pokemon.name);
+        listItem.on('click', () => showDetails(pokemon));
         pokedexList.append(listItem);
     }
 
@@ -157,7 +93,7 @@ var pokemonRepository = (function () { /*IIFE start*/
 
 })(); /*IIFE end*/
 
-pokemonRepository.loadList().then(function() { /*new code to review*/
+pokemonRepository.loadList().then(function() {
     pokemonRepository.getAll().forEach(function(pokemon){
         pokemonRepository.addListItem(pokemon);
     });
